50b26e7972d0956dd1942174f51e9fa1430dc148
danicoles <colescodes@gmail.com>
2018-02-03 09:55:13 -0800

    not complete

--- index.js
+++ index.js
@@ -1,9 +1,62 @@
-var index = require("./word.js")
+var index = require("./word.js");
+// var letter = require("./letter.js");
 
 
-var wordBank = ["Oblivion", "Skyrim", "Halo Reach", "Mass Effect"]
+var words = ["lizard", "snake", "newt", "salamander", "amphibian", "reptile", "tadpole", "axolotl"]
 
 
 var inquirer = require("inquirer");
-var letter = process.argv[2];
+// var letter = process.argv[2];
 var guessesLeft = 10
+
+
+var word = "";
+var blankWord = [];
+var arrWord = [];
+var newWord = [];
+
+function randWord() {
+    var i = Math.floor(Math.random() * 8);
+    var word = words[i].toString();
+    return word;
+}
+
+function startGame() {
+    console.log("Start game ");
+    currentWord = randWord();
+    newWord = new Word(currentWord);
+    arrWord = newWord.returnArr(newWord.word).toString();
+    blankWord = newWord.returnBlank(newWord.word).toString();
+    console.log("This is the current word: " + newWord.word);
+    console.log("This is the current array word: " + arrWord);
+    console.log("This is the current blank word: " + blankWord);
+    //console.log() to test function...
+}
+
+
+function runGame() {
+    //Question when the game starts to get the letter the user guesses.
+    inquirer.prompt([{
+        name: "character",
+        message: "Guess a letter: "
+    }]).then(response => {
+        var letter = response.character.toString();
+        var newLetter = new Letter(letter);
+        //should console.log “Correct Guess”
+        if (newLetter.check(newLetter.char, newWord.word) > -1) {
+            return newWord.guess(newWord.char, arrWord, blankWord);
+        } else {
+            return newWord.guess(newWord.char, arrWord, blankWord);
+            // return newWord.returnBlank(newWord.word)
+        }
+
+    })
+
+    //check and display current word
+    //check();
+    //recrusion to run runGame() again
+    //runGame();
+}
+
+startGame();
+runGame();
--- word.js
+++ word.js
@@ -5,49 +5,50 @@ var letter = require("./letter.js")
 //letterObjArr will hold the current word where it will then get sent through a function that
 //creates letter objects out of the word
 function Word(word) {
-   this.word = word;
-   this.returnArr = function (word) {
-       //returns the letters as a string with ‘-’ instead
-       var str = word.toString();
-       var arr = str.split("d");
-       var arrWord = [];
-       //for each statement of the specific string within wordsArr
-       arr.forEach(character function {
-           //creates a newLetter object from the Letter constructor and passes it the
-           //current character from the word that needs to be guessed.
-           var newLetter = new Letter(character);
-           arrWord.push(newLetter);
-       });
-       console.log(arrWord.toString());
-       return arrWord;
-   }
-   this.returnBlank = function(str) {
-       var blankWord = [];
-       var n = str.toString().length;
-       for (i = 0; i < n; i++) {
-           blankWord.push("-");
-       }
-       console.log(blankWord.toString());
-       return blankWord;
-   }
-   this.guess = function (character, arrWord, blankWord) {
-       //returns is guessed, updated string, etc...
-       var newLetter = new Letter(character);
-       //boolean check to see if the letter has been guessed or not...
-       var n = 0;
-       if (newLetter.bool){
-           newLetter.hasBeenGuessed();
-           n = arrWord.indexOf(newLetter);
-           console.log(n);
-           blankWord[n] = character;
-           //prints out the new blankWord with updated letter...
-           console.log(blankWord.toString());
-       } else {
-           newLetter.hasBeenGuessed();
-           n = arrWord.indexOf(newLetter);
-           blankWord[n] = arrWord[0].char;
-           //prints out the new blankWord with updated letter...
-           console.log(blankWord.toString());
-       }
-   }
+    this.word = word;
+    this.returnArr = function(word) {
+        //returns the letters as a string with "-" instead
+        var str = word.toString();
+        var arr = str.split("d");
+        var arrWord = [];
+        //for each statement of the specific string within wordsArr
+        arr.forEach(function(character)
+             {
+                //creates a newLetter object from the Letter constructor and passes it the
+                //current character from the word that needs to be guessed.
+                var newLetter = new Letter(character);
+                arrWord.push(newLetter);
+            });
+        console.log(arrWord.toString());
+        return arrWord;
+    }
+    this.returnBlank = function(str) {
+        var blankWord = [];
+        var n = str.toString().length;
+        for (i = 0; i < n; i++) {
+            blankWord.push("-");
+        }
+        console.log(blankWord.toString());
+        return blankWord;
+    }
+    this.guess = function(character, arrWord, blankWord) {
+        //returns is guessed, updated string, etc...
+        var newLetter = new Letter(character);
+        //boolean check to see if the letter has been guessed or not...
+        var n = 0;
+        if (newLetter.bool) {
+            newLetter.hasBeenGuessed();
+            n = arrWord.indexOf(newLetter);
+            console.log(n);
+            blankWord[n] = character;
+            //prints out the new blankWord with updated letter...
+            console.log(blankWord.toString());
+        } else {
+            newLetter.hasBeenGuessed();
+            n = arrWord.indexOf(newLetter);
+            blankWord[n] = arrWord[0].char;
+            //prints out the new blankWord with updated letter...
+            console.log(blankWord.toString());
+        }
+    }
 };
